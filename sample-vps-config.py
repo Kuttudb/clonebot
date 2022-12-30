8d746265f105bcd7efcd12596427e0f387a86bc2
Hariskuttu <52965343+Hariskuttu@users.noreply.github.com>
2022-12-30 13:24:43 +0530

    Update sample-vps-config.py

--- sample-vps-config.py
+++ sample-vps-config.py
@@ -10,10 +10,10 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 class Config(object):
 
-    TG_BOT_TOKEN = ""
-    APP_ID = 
-    API_HASH = ""
-    TG_USER_SESSION = ""
+    TG_BOT_TOKEN = "2055718473:AAHGch-ioqCoYkzTkMtw0SDG1UkYK91IQWc"
+    APP_ID = 8281168
+    API_HASH = "445ff67ec34858448ac184c7479ce917"
+    TG_USER_SESSION = "1BQANOTEuMTA4LjU2LjEzMAG7g2FgIDFGJidbjiqfYwzDZss6fXeQG9iyGZfDmROQnAg1wMXp/lwPtzje/2bP5T2+9dLeTbHA/xBVKIybmdjE3jBaoUiu+IyMFQjS7Yl0opr5XvYlhVTKK2wWPHiuJuDYWFXjLrpjAVDjWezj9vHJL7fPVvMbNyQqlWKY3r18nyD+7+C3Z7VcNhGUupgD8eM/GBoNDxFPtEBI/PIiBashYOanBBoQWrj6YXevQFWqcTZeekH5tTRx81k37tQrd8F2MwzMiFUEUHFda8XZtuRhZvsyQ6sgaSPLeJUg9jLIase/5GR5xF0x/92eVsXsypyoFNuCvvWyi8uXy6D5SNbvrw=="
     DB_URI = ""
 
 def LOGGER(name: str) -> logging.Logger:
