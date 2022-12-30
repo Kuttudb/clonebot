8ef0a5681b137b165de492aa611147f9a67c2d41
Hariskuttu <52965343+Hariskuttu@users.noreply.github.com>
2022-12-30 13:26:56 +0530

    Update sample-vps-config.py

--- sample-vps-config.py
+++ sample-vps-config.py
@@ -14,7 +14,7 @@ class Config(object):
     APP_ID = 8281168
     API_HASH = "445ff67ec34858448ac184c7479ce917"
     TG_USER_SESSION = "1BQANOTEuMTA4LjU2LjEzMAG7g2FgIDFGJidbjiqfYwzDZss6fXeQG9iyGZfDmROQnAg1wMXp/lwPtzje/2bP5T2+9dLeTbHA/xBVKIybmdjE3jBaoUiu+IyMFQjS7Yl0opr5XvYlhVTKK2wWPHiuJuDYWFXjLrpjAVDjWezj9vHJL7fPVvMbNyQqlWKY3r18nyD+7+C3Z7VcNhGUupgD8eM/GBoNDxFPtEBI/PIiBashYOanBBoQWrj6YXevQFWqcTZeekH5tTRx81k37tQrd8F2MwzMiFUEUHFda8XZtuRhZvsyQ6sgaSPLeJUg9jLIase/5GR5xF0x/92eVsXsypyoFNuCvvWyi8uXy6D5SNbvrw=="
-    DB_URI = ""
+    DB_URI = "mongodb+srv://123:123@cluster0.fr6gfgg.mongodb.net/?retryWrites=true&w=majority"
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
